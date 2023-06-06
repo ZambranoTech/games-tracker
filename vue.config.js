aaffef9c3964b7c7d96578295094019357a7e1ce
Javie <a2017garciaj@gmail.com>
2023-06-06 20:08:44 +0200

    añadido seccion de mis colecciones y modal en juego y mis colecciones para agregar una Coleccion personalizada

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/proyectos/2023/javiergarcia'
 })
